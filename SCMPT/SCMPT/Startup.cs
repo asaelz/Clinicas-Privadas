159e63e2ad3b9aa611384816ff8b70d915bd4293
Asael Zuniga <asael_zunigalml@yahoo.es>
2016-09-20 18:36:08 -0600

    Login

---
+++ SCMPT/SCMPT/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SCMPT.Startup))]
+namespace SCMPT
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
